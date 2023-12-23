cdda510dfb7c9f8b92486c4fc9e7c31db9511680
Tahaouad <tahaouad04@gmail.com>
2023-12-23 02:36:22 +0100

    Post+cmnt+APi style

--- apis_tp/app1/src/List_comments.jsx
+++ apis_tp/app1/src/List_comments.jsx
@@ -1,45 +1,39 @@
 import axios from 'axios';
-const { useState } = require("react")
+import { useState } from "react";
 
 const List_comments = (props) => {
-    const [Comments, setComments] = useState([])
-    const handleComments = () => {
-        const getData = async () => {
-            const my_Comments = await axios.get('https://jsonplaceholder.typicode.com/comments')
-            setComments(my_Comments.data.filter(c => c.postId == props.post.id))
-        }
-        getData()
-        props.handleChangeActivitedId(props.post.id)
-    }
-    return (
-        <>
-            {
-                    <div className='card w-75 m-auto p-3'>
-                        {props.post.id}
-                        <h3>
-                            {props.post.title}
-                        </h3>
-                        <p>
-                            {props.post.body}
-                        </p>
-                    </div>
-                
-            }
-            <div>
-                <button type='button' onClick={handleComments}>Comments</button>
-            {
-                props.activatedId==props.post.id&&(
-                    Comments.map(c=>
-                        <div className='card w-75 mb-5 mt-1 m-auto bg-info p-3  '>
-                            <h6>{c.email}</h6>
-                            <h3>{c.name}</h3>
-                            <h3>{c.body}</h3>
-                        </div>
-                        )
-                )
-            }
-            </div>
-        </>
-    )
-}
-export default List_comments ;
+  const [Comments, setComments] = useState([]);
+
+  const handleComments = () => {
+    const getData = async () => {
+      const my_Comments = await axios.get('https://jsonplaceholder.typicode.com/comments');
+      setComments(my_Comments.data.filter(c => c.postId == props.post.id));
+    };
+    getData();
+    props.handleChangeActivitedId(props.post.id);
+  };
+
+  return (
+    <>
+      <div className='card w-75 m-auto p-3'>
+        <h5 className='card-title'>{props.post.title}</h5>
+        <p className='card-text'>{props.post.body}</p>
+        <button type='button' className='btn btn-primary w-25' onClick={handleComments}>
+          Comments
+        </button>
+      </div>
+
+      {props.activatedId === props.post.id && (
+        Comments.map(c => (
+          <div key={c.id} className='card w-75 mb-5 mt-1 m-auto bg-dark p-3'>
+            <h6 className='card-subtitle mb-2 text-white'>{c.email}</h6>
+            <h5 className='card-title text-white'>{c.name}</h5>
+            <p className='card-text text-white'>{c.body}</p>
+          </div>
+        ))
+      )}
+    </>
+  );
+};
+
+export default List_comments;
