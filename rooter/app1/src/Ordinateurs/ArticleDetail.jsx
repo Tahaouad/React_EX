72ececedea78c92ba1c5f6ef72924050f090d798
Tahaouad <tahaouad04@gmail.com>
2024-01-01 23:30:24 +0100

    Router

--- rooter/app1/src/Ordinateurs/ArticleDetail.jsx
+++ rooter/app1/src/Ordinateurs/ArticleDetail.jsx
@@ -18,11 +18,7 @@ const ArticleDetail = ({ ordinateurs }) => {
           className="card-img w-50"/>  
         <div className="card-body">
           <h5 className="card-title">{article.libelle}</h5>
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
+          
         </div>
       </div>
     </div>
