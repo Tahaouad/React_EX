ec99da4a811686ed5f931109e766c7f317217037
Tahaouad <tahaouad04@gmail.com>
2024-01-02 23:51:56 +0100

    Raja shop

--- router3_tp/src/components/Products.jsx
+++ router3_tp/src/components/Products.jsx
@@ -1,13 +1,115 @@
 import {Link } from 'react-router-dom';
 import Navbar from './Navbar';
+import ImageSlider from './ImageSlider';
+import ProductSlider from './ProductsSlide';
 
+const images=['slide-a-1.png','slide-3-1.png','slide-b-1.png']
 const Products=({products})=>{
-    
+    const prod = [
+        {
+        id:1,
+        image:'Tenu1.PNG',
+        libelle :'Maillot Domicile Green',
+        prix : '490.00 DH'
+        },
+        {
+          id:2,
+          image:'Tenu2.PNG',
+          libelle :'Maillot Domicile White',
+          prix : '490.00 DH'
+        },
+        {
+          id:3,
+          image:'Tenu3PNG.PNG',
+          libelle :'Polo Raja club Black',
+          prix : '300.00 DH'
+        },
+        {
+          id:4,
+          image:'Tenu4PNG.PNG',
+          libelle :'Polo Raja club Green',
+          prix : '300.00 DH'
+        },
+        {
+          id:5,
+          image:'Tenu5PNG.PNG',
+          libelle :'Maillot Domicile Black',
+          prix : '500.00 DH'
+        },
+        {
+          id:6,
+          image:'Pull1.PNG',
+          libelle :'Pull white Raja club',
+          prix : '299.00 DH'
+        },
+        
+        {
+          id:7,
+          image:'Pull2PNG.PNG',
+          libelle :'Pull Gray Raja club',
+          prix : '299.00 DH'
+        },
+        {
+          id:6,
+          image:'Pull1.PNG',
+          libelle :'Pull white Raja club',
+          prix : '299.00 DH'
+        },{
+          id:1,
+          image:'Tenu1.PNG',
+          libelle :'Maillot Domicile Green',
+          prix : '490.00 DH'
+          },
+          {
+            id:2,
+            image:'Tenu2.PNG',
+            libelle :'Maillot Domicile White',
+            prix : '490.00 DH'
+          },
+          {
+            id:3,
+            image:'Tenu3PNG.PNG',
+            libelle :'Polo Raja club Black',
+            prix : '300.00 DH'
+          },
+          {
+            id:4,
+            image:'Tenu4PNG.PNG',
+            libelle :'Polo Raja club Green',
+            prix : '300.00 DH'
+          },
+          {
+            id:5,
+            image:'Tenu5PNG.PNG',
+            libelle :'Maillot Domicile Black',
+            prix : '500.00 DH'
+          },
+          {
+            id:6,
+            image:'Pull1.PNG',
+            libelle :'Pull white Raja club',
+            prix : '299.00 DH'
+          },
+          
+          {
+            id:7,
+            image:'Pull2PNG.PNG',
+            libelle :'Pull Gray Raja club',
+            prix : '299.00 DH'
+          },
+          {
+            id:6,
+            image:'Pull1.PNG',
+            libelle :'Pull white Raja club',
+            prix : '299.00 DH'
+          }]
     
     return(
         <>
         <Navbar />
-        <img className='img w-100 mb-3' src={require('../images/cover.PNG')} />
+        <ImageSlider images={images} autoSlideInterval={4000} />
+        <ProductSlider items={prod}/>
+        <img src={require(`../images/cover.PNG`)} alt=""  width='100%'/>
         <h1 className='m-auto fw-bold text-center text-success '>EXPLORATION EXCLUSIVE</h1>
         <h6 className='m-auto text-center fw-lighter'>Les dernières tendances du Raja</h6>
         <div className="d-flex flex-wrap w-100 justify-content mt-3">
